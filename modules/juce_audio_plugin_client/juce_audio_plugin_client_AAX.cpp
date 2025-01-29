9034766c1b598aba51edc9e3eba7ef427f0ea06b
Andrew Scott <andrew@submissionaudio.com>
2025-01-30 09:58:27 +1100

    AAX: AddProperty (AAX_eProperty_RequiresChunkCallsOnMainThread, true);

--- modules/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp
+++ modules/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp
@@ -2520,6 +2520,13 @@ namespace AAXClasses
         properties->AddProperty (AAX_eProperty_SupportsSaveRestore, false);
        #endif
 
+        //--------------------------------------------------------------------------
+        // MOD submissionaudio
+
+        properties->AddProperty (AAX_eProperty_RequiresChunkCallsOnMainThread, true);
+
+        //--------------------------------------------------------------------------
+
         properties->AddProperty (AAX_eProperty_ObservesTransportState, true);
 
         if (fullLayout.getChannelSet (true, 1) == AudioChannelSet::mono())
